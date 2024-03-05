2eda4d4d7f6abe0e362e58258e49aeba0c7630eb
“rubng87” <gallegoruben_srye@hotmail.com>
2024-03-05 10:49:34 +0100

    arrays

--- arrays.js
+++ arrays.js
@@ -1,6 +1,14 @@
 // ARRAYS
 
-let arrayFrutas = ["pera", "kiwi", "mango"]
+let arrayFrutas = ["pera", "kiwi", ,"mango"]
 
 let array1 = new Array(3)
+let array2 = []
 console.log(array1.length);
+
+//length <-- para contar los elementos
+console.log(arrayFrutas.length);
+
+//para acceder a cada eemento utilizamos su indice
+//que empieza a contar desde 0
+console.log(arrayFrutas[2]);
